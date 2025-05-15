5cb4415b8e8e7c209725cff52aa144c84577f299
hfziu <8758566+hfziu@users.noreply.github.com>
2025-05-15 13:38:52 +0800

    set alternative ssh-agent

--- Linux/zshrc.zsh
+++ Linux/zshrc.zsh
@@ -32,3 +32,16 @@ alias mv='mv -i'
 # uv (github.com/astral-sh/uv)
 # ----------------------------
 (( $+commands[uv] )) && eval "$(uv generate-shell-completion zsh)" && eval "$(uvx --generate-shell-completion zsh)"
+
+# =============================
+# Linux Workstation - SSH Agent
+# =============================
+# The ssh-agent.service (systemd) included with openssh creates a socket at $XDG_RUNTIME_DIR/ssh-agent.socket.
+# - If this is not an SSH session, we try to use the socket created by ssh-agent.service.
+# - Otherwise for an SSH session, the SSH agent might have been forwarded from the client and the SSH_AUTH_SOCK variable is set accordingly.
+#     We don't overwrite it.
+if [ -z "$SSH_CONNECTION" ]; then
+    if [ -e "$XDG_RUNTIME_DIR/ssh-agent.socket" ]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+    fi
+fi
