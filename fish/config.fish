a0679f32469539e58789dbf29cc44534006d3f3c
hfziu <8758566+hfziu@users.noreply.github.com>
2025-12-18 17:40:07 +0800

    prefer forwarded SSH agent over Bitwarden SSH Agent

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,20 @@
 if status is-interactive
     set -l os (uname)
     
-    # Bitwarden SSH Agent
+    # SSH Agent setup
     set -l bw_socks "$HOME/.bitwarden-ssh-agent.sock"
     test "$os" = Darwin
         and set bw_socks "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
         or set -p bw_socks "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
     for sock in $bw_socks
         test -S "$sock"; and set -gx SSH_AUTH_SOCK "$sock"; and break
+    end
+    # Prefer forwarded SSH agent in SSH sessions
+    if set -q SSH_TTY
+        set -l forwarded_sock /run/user/1000/gnupg/S.gpg-agent.ssh
+        if test -S "$forwarded_sock"
+            set -gx SSH_AUTH_SOCK "$forwarded_sock"
+        end
     end
 
     # Homebrew setup
